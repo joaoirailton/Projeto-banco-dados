382d4bae95489492319686fd9c1af8e8037e45fd
joaoirailton <junioramos4991@gmail.com>
2025-06-24 12:24:41 -0300

    criei um arquivo js

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+//COLOCAR EVENTOS NOS BOTÕES
+const btnSalvar = window.document.getElementById('salvar')
+const btnPesquisar = window.document.getElementById('pesquisar')
+
+
+
+function salvar(){
+    window.alert('ola')
+}
