ebf88cfb44ec1b281b910d7da79a4dadee28a26b
joaoirailton <>
2025-07-10 12:42:30 -0300

    ok

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,14 @@
 //DECLARAÇÕES
-const body = document.body;
-const header = document.header;
+const conteudo = document.body;
+const menu = document.getElementsByTagName('header');
 
+//ADICIONA EVENTO
+menu[0].addEventListener('mouseover', abrir)
+menu[0].addEventListener('mouseout', ()=>{
+    conteudo.removeAttribute('id', 'body')
+})
+
+//FUNÇÃO
+function abrir(){
+    conteudo.setAttribute('id', 'body');
+}   
